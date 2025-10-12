6e0f64105c719021913c5e83f936a7847d6328fb
Will <willlu2@illinois.edu>
2025-10-11 21:11:00 -0500

    add superuser

--- backend/applications/admin.py
+++ backend/applications/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Company, Job
 
-# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'website', 'created_at')
+    search_fields = ('name',)
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'status', 'created_at')
+    list_filter = ('status',)
+    search_fields = ('title',)

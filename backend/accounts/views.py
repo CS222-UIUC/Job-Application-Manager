5574bba91a78248bf883df48fd095b6074e8377f
Will <willlu2@illinois.edu>
2025-10-26 23:20:58 -0500

    fix the import order

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,9 @@
+from django.contrib.auth import login, logout
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import login, logout
 from .serializers import (
     UserRegistrationSerializer,
     UserLoginSerializer,
